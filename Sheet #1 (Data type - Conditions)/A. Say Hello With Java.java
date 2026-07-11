bd1eb706c13fd3be89534eecc9029e74dd0d0221
Isagi Yoichi <iamisagiyoichi011@gmail.com>
2026-07-12 00:01:54 +0530

    Add Java program to greet user by name

---
+++ Sheet #1 (Data type - Conditions)/A. Say Hello With Java.java
@@ -0,0 +1,15 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+public class main
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner sc = new Scanner(System.in);
+		String s = sc.next();
+		System.out.println("Hello, "+s);
+		sc.close();
+	}
+}
