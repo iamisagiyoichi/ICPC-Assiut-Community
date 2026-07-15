694eb6f3ddb7dfb2ce49255a7b5ebcb7bc63c102
Isagi Yoichi <iamisagiyoichi011@gmail.com>
2026-07-16 00:00:03 +0530

    Update G. Summation from 1 to N.java

--- Sheet #1 (Data type - Conditions)/G. Summation from 1 to N.java
+++ Sheet #1 (Data type - Conditions)/G. Summation from 1 to N.java
@@ -3,7 +3,7 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
+        long n = sc.nextLong();
         System.out.println(n*(n+1)/2);
     }
 }
