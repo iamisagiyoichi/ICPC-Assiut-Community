e5ca65917ba58ca892faf6654521feac1b345257
Isagi Yoichi <iamisagiyoichi011@gmail.com>
2026-07-16 22:43:05 +0530

    Add Java program to determine point coordinates

---
+++ Sheet #1 (Data type - Conditions)/Q. Coordinates of a Point.java
@@ -0,0 +1,43 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    static class FastScanner {
+        private final BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        private StringTokenizer st;
+
+        String next() throws IOException {
+            while (st == null || !st.hasMoreElements()) {
+                st = new StringTokenizer(br.readLine());
+            }
+            return st.nextToken();
+        }
+
+        double nextDouble() throws IOException {
+            return Double.parseDouble(next());
+        }
+    }
+
+    public static void main(String[] args) throws Exception {
+        FastScanner fs = new FastScanner();
+
+        double x = fs.nextDouble();
+        double y = fs.nextDouble();
+
+        if (x == 0 && y == 0) {
+            System.out.println("Origem");
+        } else if (x == 0) {
+            System.out.println("Eixo Y");
+        } else if (y == 0) {
+            System.out.println("Eixo X");
+        } else if (x > 0 && y > 0) {
+            System.out.println("Q1");
+        } else if (x < 0 && y > 0) {
+            System.out.println("Q2");
+        } else if (x < 0 && y < 0) {
+            System.out.println("Q3");
+        } else {
+            System.out.println("Q4");
+        }
+    }
+}
