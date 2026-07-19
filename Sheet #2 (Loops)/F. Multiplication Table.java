5d5bb0580f1d2e2edb0f0238e26e1ec2430ec493
Isagi Yoichi <iamisagiyoichi011@gmail.com>
2026-07-19 23:36:56 +0530

    Create F. Multiplication Table.java

---
+++ Sheet #2 (Loops)/F. Multiplication Table.java
@@ -0,0 +1,71 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    static class FastReader {
+        private final BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        private StringTokenizer st;
+
+        String next() throws IOException {
+            while (st == null || !st.hasMoreElements()) {
+                st = new StringTokenizer(br.readLine());
+            }
+            return st.nextToken();
+        }
+
+        int nextInt() throws IOException {
+            return Integer.parseInt(next());
+        }
+
+        long nextLong() throws IOException {
+            return Long.parseLong(next());
+        }
+
+        double nextDouble() throws IOException {
+            return Double.parseDouble(next());
+        }
+
+        String nextLine() throws IOException {
+            return br.readLine();
+        }
+
+        int[] nextIntArray(int n) throws IOException {
+            int[] a = new int[n];
+            int b = 0;
+            while (b < n) {
+                a[b] = nextInt();
+                b++;
+            }
+            return a;
+        }
+
+        long[] nextLongArray(int n) throws IOException {
+            long[] a = new long[n];
+            int b = 0;
+            while (b < n) {
+                a[b] = nextLong();
+                b++;
+            }
+            return a;
+        }
+    }
+
+    static FastReader sc = new FastReader();
+    static PrintWriter out = new PrintWriter(System.out);
+
+    public static void solve() throws Exception {
+        int a = sc.nextInt();
+
+        int b = 1;
+        while (b <= 12) {
+            out.println(a + " * " + b + " = " + (a * b));
+            b++;
+        }
+    }
+
+    public static void main(String[] args) throws Exception {
+        solve();
+        out.flush();
+    }
+}
