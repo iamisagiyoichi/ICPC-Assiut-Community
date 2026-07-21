6d63164009983b901611286f823f7ad9bd01762b
Isagi Yoichi <iamisagiyoichi011@gmail.com>
2026-07-21 23:22:33 +0530

    Create P. Shape1.java

---
+++ Sheet #2 (Loops)/P. Shape1.java
@@ -0,0 +1,79 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    static class FastReader {
+        private final BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        private StringTokenizer st;
+
+        String next() throws IOException {
+            while (st == null || !st.hasMoreElements()) {
+                st = new StringTokenizer(br.readLine());
+            }
+            return st.nextToken();
+        }
+
+        int nextInt() throws IOException {
+            return Integer.parseInt(next());
+        }
+
+        long nextLong() throws IOException {
+            return Long.parseLong(next());
+        }
+
+        double nextDouble() throws IOException {
+            return Double.parseDouble(next());
+        }
+
+        String nextLine() throws IOException {
+            return br.readLine();
+        }
+
+        int[] nextIntArray(int n) throws IOException {
+            int[] a = new int[n];
+            int b = 0;
+            while (b < n) {
+                a[b] = nextInt();
+                b++;
+            }
+            return a;
+        }
+
+        long[] nextLongArray(int n) throws IOException {
+            long[] a = new long[n];
+            int b = 0;
+            while (b < n) {
+                a[b] = nextLong();
+                b++;
+            }
+            return a;
+        }
+    }
+
+    static FastReader sc = new FastReader();
+    static PrintWriter out = new PrintWriter(System.out);
+
+    static void printRow(int a) {
+        int b = 0;
+        while (b < a) {
+            out.print("*");
+            b++;
+        }
+        out.println();
+    }
+
+    public static void solve() throws Exception {
+        int a = sc.nextInt();
+
+        while (a > 0) {
+            printRow(a);
+            a--;
+        }
+    }
+
+    public static void main(String[] args) throws Exception {
+        solve();
+        out.flush();
+    }
+}
