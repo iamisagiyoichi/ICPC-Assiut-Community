4e40e8ba5211d7eedb62a892bc1394d495e21237
Isagi Yoichi <iamisagiyoichi011@gmail.com>
2026-07-26 23:58:03 +0530

    Create T. Shape3.java

---
+++ Sheet #2 (Loops)/T. Shape3.java
@@ -0,0 +1,57 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    static class FastReader {
+        private final BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        private StringTokenizer st;
+
+        String next() throws IOException {
+            while (st == null || !st.hasMoreElements()) {
+                st = new StringTokenizer(br.readLine());
+            }
+            return st.nextToken();
+        }
+
+        int nextInt() throws IOException {
+            return Integer.parseInt(next());
+        }
+    }
+
+    static FastReader sc = new FastReader();
+    static PrintWriter out = new PrintWriter(System.out);
+
+    static void printRow(int s, int st) {
+        int i = 0;
+
+        while (i < s) {
+            out.print(" ");
+            i++;
+        }
+
+        i = 0;
+        while (i < st) {
+            out.print("*");
+            i++;
+        }
+
+        out.println();
+    }
+
+    public static void solve() throws Exception {
+        int n = sc.nextInt();
+
+        int i = 1;
+
+        while (i <= n) {
+            printRow(n - i, 2 * i - 1);
+            i++;
+        }
+    }
+
+    public static void main(String[] args) throws Exception {
+        solve();
+        out.flush();
+    }
+}
