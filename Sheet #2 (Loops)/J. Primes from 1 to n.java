6f89e8d6caffd124e335eaa2b84bb6e9899fa580
Isagi Yoichi <iamisagiyoichi011@gmail.com>
2026-07-29 23:55:11 +0530

    Remove unnecessary blank line in J. Primes from 1 to n.java

--- Sheet #2 (Loops)/J. Primes from 1 to n.java
+++ Sheet #2 (Loops)/J. Primes from 1 to n.java
@@ -2,7 +2,6 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-
     static class FastReader {
         private final BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         private StringTokenizer st;
