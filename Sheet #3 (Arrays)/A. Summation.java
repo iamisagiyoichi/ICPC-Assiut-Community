aa501b331d50d6806ece0e8ee6af553ac276c369
Isagi Yoichi <iamisagiyoichi011@gmail.com>
2026-07-31 23:40:58 +0530

    Update A. Summation.java

--- Sheet #3 (Arrays)/A. Summation.java
+++ Sheet #3 (Arrays)/A. Summation.java
@@ -1,1 +1,57 @@
+import java.io.*;
+import java.util.*;
 
+public class Main {
+
+    static class FastReader {
+        private final BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        private StringTokenizer st;
+
+        String next() throws IOException {
+            while (st == null || !st.hasMoreTokens()) {
+                st = new StringTokenizer(br.readLine());
+            }
+            return st.nextToken();
+        }
+
+        int nextInt() throws IOException {
+            return Integer.parseInt(next());
+        }
+
+        long nextLong() throws IOException {
+            return Long.parseLong(next());
+        }
+
+        long[] nextLongArray(int n) throws IOException {
+            long[] a = new long[n];
+            int b = 0;
+            while (b < n) {
+                a[b] = nextLong();
+                b++;
+            }
+            return a;
+        }
+    }
+
+    static FastReader sc = new FastReader();
+    static PrintWriter out = new PrintWriter(System.out);
+
+    static void solve() throws Exception {
+        int n = sc.nextInt();
+
+        long s = 0;
+
+        int i = 0;
+        while (i < n) {
+            s += sc.nextLong();
+            i++;
+        }
+
+        out.println(Math.abs(s));
+    }
+
+    public static void main(String[] args) throws Exception {
+        solve();
+        out.flush();
+    }
+}
