4621467098effb93c54e7d29a201a9cc92b4ea81
Isagi Yoichi <iamisagiyoichi011@gmail.com>
2026-08-02 23:48:46 +0530

    Create C. Replacement.java

---
+++ Sheet #3 (Arrays)/C. Replacement.java
@@ -0,0 +1,51 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    static class FastReader {
+        private final BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        private StringTokenizer st;
+
+        String next() throws IOException {
+            while (st == null || !st.hasMoreTokens()) {
+                st = new StringTokenizer(br.readLine());
+            }
+            return st.nextToken();
+        }
+
+        int nextInt() throws IOException {
+            return Integer.parseInt(next());
+        }
+    }
+
+    static FastReader sc = new FastReader();
+    static PrintWriter out = new PrintWriter(System.out);
+
+    static void solve() throws Exception {
+        int n = sc.nextInt();
+
+        int i = 0;
+
+        while (i < n) {
+            int a = sc.nextInt();
+
+            if (a > 0) {
+                out.print("1 ");
+            } else if (a < 0) {
+                out.print("2 ");
+            } else {
+                out.print("0 ");
+            }
+
+            i++;
+        }
+
+        out.println();
+    }
+
+    public static void main(String[] args) throws Exception {
+        solve();
+        out.flush();
+    }
+}
